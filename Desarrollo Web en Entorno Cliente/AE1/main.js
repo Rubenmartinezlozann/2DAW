52bed4c1c2c6e0afd0ca3aa2654be0bd46b6fbfd
ruben <rubenmartinezlozano@gmail.com>
2020-09-24 11:48:56 +0200

    AE1-1

---
+++ Desarrollo Web en Entorno Cliente/AE1/main.js
@@ -0,0 +1,10 @@
+//A)
+alert("Soy el primer script");
+
+//B)
+let alertMessage="Soy el primer script";
+alert(alertMessage);
+
+//C)
+const ALERT_MESSAGE="Soy el primer script";
+alert(ALERT_MESSAGE+" y estoy funcionando sobre "+ navigator.appVersion);
