8c2a140e9a9832dd192a32c8d43a2ef47c6ff10d
ruben <rubenmartinezlozano@gmail.com>
2020-10-02 17:34:26 +0200

    AE2-6

--- Desarrollo Web en Entorno Cliente/AE2/ae2-6.js
+++ Desarrollo Web en Entorno Cliente/AE2/ae2-6.js
@@ -1,10 +1,2 @@
-function Listar(users,blackListed){
-    return users.filter(nombre=>{
-        if(blackListed.some(nombreListaNegra=>{return nombre === nombreListaNegra})){//Si el nombre está en lalista negra devolverá false, si no devolverá true
-            return false;
-        }else{
-            return true;
-        }
-    });
-}
-console.log(Listar(["a","b","c","d","e"],["b","e"]));
+const Listar = (users, blackListed) => users.filter(nombre => blackListed.some(nombreListaNegra => nombre === nombreListaNegra) ? false : true) 
+console.log(Listar(["a", "b", "c", "d", "e"], ["b", "e"]));
