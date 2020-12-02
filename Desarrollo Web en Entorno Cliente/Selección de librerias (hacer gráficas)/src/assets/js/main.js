91f6cf626ad65fa161e003183da3adab74bbcdc4
ruben <rubenmartinezlozano@gmail.com>
2020-12-02 18:45:20 +0100

    AE4-3 Añadido función actualizar gráfica

--- Desarrollo Web en Entorno Cliente/Selección de librerias (hacer gráficas)/src/assets/js/main.js
+++ Desarrollo Web en Entorno Cliente/Selección de librerias (hacer gráficas)/src/assets/js/main.js
@@ -13,75 +13,76 @@ const obtenDatos = (elemento) => {
         }
     }
     return ArrayFinal;
-} 
+}
+
+const btnGraficoPulsado = (primerClick => {
+
+    /*datasetTemp = {
+        label: 'Temperaturas (ºC)',
+        data: obtenDatos(document.getElementById('temperaturas')),
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+    }
+
+    datasetLluvias = {
+        label: 'Lluvias (mm3)',
+        data: obtenDatos(document.getElementById('lluvias')),
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+    }*/
+    // chart.labels = obtenDatos(document.getElementById('meses'));
+    //chart.data.datasets.push(datasetTemp);
+    //chart.data.dadatasets.push(datasetLluvias);
+    /* else {
+        chart.labels = obtenDatos(document.getElementById('meses'));
+        chart.data.datasets[0].data = obtenDatos(document.getElementById('temperaturas'));
+        chart.data.datasets[1].data = obtenDatos(document.getElementById('lluvias'));
+        chart.update();
+    }*/
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-    const meses = obtenDatos(document.getElementById('meses'));
-    const temperaturas = obtenDatos(document.getElementById('temperaturas'));
-    const lluvias = obtenDatos(document.getElementById('lluvias'));
     const btnGrafico = document.getElementById("btnGrafico");
-    
+    let primerClick = true;
+    btnGrafico.addEventListener("click", () => {
+        const meses = obtenDatos(document.getElementById('meses'));
+        const temperaturas = obtenDatos(document.getElementById('temperaturas'));
+        const lluvias = obtenDatos(document.getElementById('lluvias'));
 
-    let grafico = new Chart(document.getElementById('grafico').getContext('2d'), {
-        type: 'line',
-        data: {
-            labels: meses,
-            datasets: [{
-                label: 'Temperaturas (ºC)',
-                data: temperaturas,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+        let chart = new Chart(document.getElementById('grafico').getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: meses,
+                datasets: [{
+                    label: 'Temperaturas (ºC)',
+                    data: temperaturas,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1,
+                },
+                {
+                    label: 'Lluvias (mm3)',
+                    data: lluvias,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }],
             },
-            {
-                label: 'Lluvias',
-                data: lluvias,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
             }
-            ]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+        });
+        if (primerClick) {
+            btnGrafico.value = 'actualizar gráfica';
+            document.getElementById('grafico').style.display = 'block';
         }
-    });
-
-    btnGrafico.addEventListener("click", ()=>{
-        document.getElementById('grafico').style.display = 'block';
     })
 });
