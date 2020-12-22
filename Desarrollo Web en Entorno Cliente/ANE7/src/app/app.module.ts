68081d547b794dd28fbc1bb7fe0e9b9440e88ebe
ruben <rubenmartinezlozano@gmail.com>
2020-12-22 20:34:18 +0100

    Finalizado html y estilos

--- Desarrollo Web en Entorno Cliente/ANE7/src/app/app.module.ts
+++ Desarrollo Web en Entorno Cliente/ANE7/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+//import { AppRoutingModule } from './app-routing.module';
+//import { HomeViewComponent } from './home-view/home-view.component';
+//import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductoComponent,
+    //AppRoutingModule,
+    //HomeViewComponent,
+    //ProductViewComponent
   ],
   imports: [
     BrowserModule
--- Desarrollo Web en Entorno Cliente/ANE7/src/app/producto/producto.component.ts
+++ Desarrollo Web en Entorno Cliente/ANE7/src/app/producto/producto.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-producro',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
   @Input() codigo = '';
